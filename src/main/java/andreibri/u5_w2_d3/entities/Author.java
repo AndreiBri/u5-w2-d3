3a2ff3bd193800eca761d1d04c4bbdffc201f0e9
Andrei Briceag <andrei98briceag@gmail.com>
2026-04-15 15:22:24 +0200

    Enities adjustments peyload creation

--- src/main/java/andreibri/u5_w2_d3/entities/Author.java
+++ src/main/java/andreibri/u5_w2_d3/entities/Author.java
@@ -1,14 +1,23 @@
 package andreibri.u5_w2_d3.entities;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
-import java.util.List;
 import java.util.UUID;
 
 @Entity
 @Table(name = "authors")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Author {
 
     @Id
@@ -21,64 +30,4 @@ public class Author {
     private LocalDate birthDate;
     private String avatar;
 
-    @OneToMany(mappedBy = "author")
-    @JsonIgnore
-    private List<BlogPost> blogPosts;
-
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public LocalDate getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(LocalDate birthDate) {
-        this.birthDate = birthDate;
-    }
-
-    public String getAvatar() {
-        return avatar;
-    }
-
-    public void setAvatar(String avatar) {
-        this.avatar = avatar;
-    }
-
-    public List<BlogPost> getBlogPosts() {
-        return blogPosts;
-    }
-
-    public void setBlogPosts(List<BlogPost> blogPosts) {
-        this.blogPosts = blogPosts;
-    }
 }
--- src/main/java/andreibri/u5_w2_d3/entities/BlogPost.java
+++ src/main/java/andreibri/u5_w2_d3/entities/BlogPost.java
@@ -1,11 +1,19 @@
 package andreibri.u5_w2_d3.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.UUID;
 
 @Entity
 @Table(name = "blog_posts")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class BlogPost {
 
     @Id
@@ -22,59 +30,4 @@ public class BlogPost {
     @JoinColumn(name = "author_id")
     private Author author;
 
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public int getReadingTime() {
-        return readingTime;
-    }
-
-    public void setReadingTime(int readingTime) {
-        this.readingTime = readingTime;
-    }
-
-    public String getCover() {
-        return cover;
-    }
-
-    public void setCover(String cover) {
-        this.cover = cover;
-    }
-
-    public Author getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(Author author) {
-        this.author = author;
-    }
 }
